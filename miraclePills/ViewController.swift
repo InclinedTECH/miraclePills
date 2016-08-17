a9ddc7606712afb79632ffe47da9fdeb56d65443
Nicole Bolden <jnjbolden@print.epsonconnect.com>
2016-08-17 14:06:25 -0500

    Initial Commit

---
+++ miraclePills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  miraclePills
+//
+//  Created by Nicole Bolden on 8/17/16.
+//  Copyright © 2016 Inclined Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
