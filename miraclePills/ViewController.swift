a92b21621ee645f48b750662dc27989c21ee6a07
Nicole Bolden <nicolebolden@nicoles-mbp.attlocal.net>
2016-08-18 07:20:38 -0500

    state picker

--- miraclePills/ViewController.swift
+++ miraclePills/ViewController.swift
@@ -1,6 +1,5 @@
 //
-//  ViewController.swift
-//  miraclePills
+//  ViewController.swift//  miraclePills
 //
 //  Created by Nicole Bolden on 8/17/16.
 //  Copyright © 2016 Inclined Tech. All rights reserved.
@@ -8,11 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+   
+    @IBOutlet weak var statePickerBtn: UIButton!
+    @IBOutlet weak var statePicker: UIPickerView!
+    let states = ["Alabama", "Alaska","Arizona","Arkansas","California","Colorado","Conneticut", "Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusettes","Michican","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virgina","Washington","West Virgina","Wisconsin","Wyoming"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func stateBtnPressed(_ sender: AnyObject) {
+        statePicker.isHidden = false
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
 }
 
